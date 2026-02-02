2010d09ca373c37264f3e1f6899dff8763ea8f94
Julio C Lopez <90801533+julio-lopezsanz@users.noreply.github.com>
2026-02-02 11:54:03 -0600

    Agrega reto: 2 contador real

---
+++ Retos python/_2contador_real.py
@@ -0,0 +1,31 @@
+"""
+words = ["python", "java", "python", "c++", "java", "python"]
+
+Devuelve:
+{
+  "python": 3,
+  "java": 2,
+  "c++": 1
+}
+
+Reglas:
+- no count()
+- no librerías
+"""
+
+words = ["python", "java", "python", "c++", "java", "python"]
+
+def word_counter(words_list):
+    """
+    Cuenta cuantas veces se repite un elementos, dentro de una lista
+    y devuelve un diccionario con el resultado
+    """
+    #Crea el diccionario vacio donde se almacenara el resultado
+    counts = {}
+
+    for word in words_list:
+        counts[word] = counts.get(word, 0) + 1
+
+    return counts
+
+print(word_counter(words))
