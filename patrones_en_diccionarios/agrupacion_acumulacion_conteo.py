c8607117325fe6bbdb702dfccc36848325ef4425
Julio C Lopez <90801533+julio-lopezsanz@users.noreply.github.com>
2026-02-12 11:17:31 -0600

    Agrega ejercicio: agrupacion acumulacion conteo

---
+++ patrones_en_diccionarios/agrupacion_acumulacion_conteo.py
@@ -0,0 +1,33 @@
+"""
+Tienes una lista de tuplas (usuario, monto) y quieres:
+
+un diccionario donde cada usuario tenga:
+    - el total gastado
+    - el número de compras
+"""
+
+sales = [
+    ("Ana",40),
+    ("Luis",50),
+    ("Ana",40),
+    ("Ana",40)
+]
+
+def purchases_per_person(sales_data):
+    """
+    Devuelve un diccionario con:
+        - monto total de compras y cantidad total de compras por persona
+    """
+
+    grouped = {}
+
+    for user, amount in sales_data:
+        if user not in grouped:
+            grouped[user] = {"total": 0, "purchases": 0}
+
+        grouped[user]["total"] += amount
+        grouped[user]["purchases"] += 1
+
+    return grouped
+
+print(purchases_per_person(sales))
