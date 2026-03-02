43ff7bc86bd35cde0e56468b8095b3d1059e440d
Julio C Lopez <90801533+julio-lopezsanz@users.noreply.github.com>
2026-03-02 11:10:38 -0600

    Agrega ejercicio: intercambio_variables

---
+++ 1_Ejercicios_Basicos/intercambio_variables.py
@@ -0,0 +1,13 @@
+"""
+Crea dos variables a = 10 y b = 20. 
+Intercambia sus valores sin usar una variable temporal. 
+Imprime los valores antes y después del intercambio.
+"""
+
+A = 10
+B = 20
+print(A)
+print(B)
+A,B = B,A
+print(A)
+print(B)
