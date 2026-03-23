6a1173937e5d19ebf0c8570fca1c47c3f88248f8
Julio C Lopez <90801533+julio-lopezsanz@users.noreply.github.com>
2026-03-23 09:29:50 -0600

    Agrega ejercicio: lista

---
+++ 2_Listas_Diccionarios_Sets/Bloque1/lista.py
@@ -0,0 +1,14 @@
+"""
+Crea una lista con 5 nombres. 
+    - Agrega uno nuevo 
+    - Elimina el tercero  
+    - Imprime la lista final.
+"""
+
+user_name_list = ["Julio", "Maria", "Adolfo", "Argenis", "Rafael"]
+
+user_name_list.append("Cesar")
+
+user_name_list.remove("Adolfo")
+
+print(user_name_list)
