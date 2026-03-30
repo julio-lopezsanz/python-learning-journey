b6011872d58610df6a39d1f4dca8ced91f08e96d
Julio C Lopez <90801533+julio-lopezsanz@users.noreply.github.com>
2026-03-30 11:01:33 -0600

    Agrega ejercicio: encapsulamiento

---
+++ 3.POO/Bloque1/encapsulamiento.py
@@ -0,0 +1,25 @@
+"""
+Crea una clase Usuario con atributos públicos nombre y email, 
+y un atributo privado __password. 
+Agrega un método cambiar_password(actual, nueva) 
+que solo cambie la contraseña si la actual es correcta.
+"""
+
+class User:
+    """Registra un usuario, con un email y su contraseña"""
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.__password = password
+
+    def change_password(self, current_password, new_password):
+        """Cambia la contraseña de un usuario, siempre y cuando escriba su actual contraseña"""
+        if current_password == self.__password:
+            self.__password = new_password
+            print(f"La contraseña ha cambiado exitosamente")
+        else:
+            print("Contraseña actual incorrecta")
+
+usuario = User("Julio","julio@email.com","holaamigo")
+usuario.change_password("soyjulio","nosequeponer")
+usuario.change_password("holaamigo","holajulio")
