7abaa84e57d443b1bccbdbf28c94333d27a23416
Julio C Lopez <90801533+julio-lopezsanz@users.noreply.github.com>
2026-06-15 10:29:49 -0600

    feat: Agregar ejercicio_3

---
+++ 1_Ejercicios_Basicos/ejercicio_3.py
@@ -0,0 +1,30 @@
+"""
+Author: Julio Cesar Lopez Sanchez
+Problema: Mostrar solo aquellos caracteres que estén presentes en un índice 
+          par en la cadena dada.
+"""
+
+def mostrar_indices_pares(word) -> None:
+    """Muestra los caracteres de una cadena con indice par
+
+    toma una cadena de caracteres, la recorre con el uso del ciclo for, e imprime
+    unicamente los caracteres que esten en un indice par.
+
+    args:
+        word: Cadena de texto a procesar.
+
+    returns:
+            None   
+    """
+
+    for i, char in enumerate(word):
+        if i % 2 == 0:
+            print(f"{char}")
+
+def main() -> None:
+    """Funcion principal"""
+    word = input("Ingresa una palabra a desglosar: ")
+    mostrar_indices_pares(word)
+
+if __name__ == "__main__":
+    main()
